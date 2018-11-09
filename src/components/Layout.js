938500ca74a52abbde04be0858e9f73e20575f3b
Viljar Kärgenberg <viljark@gmail.com>
2018-11-10 00:39:23 +0200

    add initial meta tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,16 @@ import './all.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Mesilasemade kasvatus ja müük" />
+    <Helmet>
+      <title>Mesilasemade kasvatus ja müük</title>
+      <meta name="description" content="Mesilasemade kasvatus ja müük" />
+      {/*<meta name="image" content={image} />*/}
+
+      {/* OpenGraph tags */}
+      <meta property="og:url" content="https://emadekasvatus.ee" />
+      <meta property="og:title" content="Mesilasemade kasvatus ja müük" />
+      <meta property="og:description" content="Mesilasemade kasvatus ja müük" />
+    </Helmet>
     <Navbar />
     <div>{children}</div>
   </div>
