2911529359ac8eb423f9d3621bca6514a0fafe7f
Viljar Kärgenberg <viljark@gmail.com>
2018-11-10 00:49:05 +0200

    add favicon, cleanup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react";
+import Helmet from "react-helmet";
 
-import Navbar from '../components/Navbar'
-import './all.scss'
+import Navbar from "../components/Navbar";
+import favicon from "../img/favicon.ico";
+import "./all.scss";
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
       <title>Mesilasemade kasvatus ja müük</title>
-      <meta name="description" content="Mesilasemade kasvatus ja müük" />
+      <link rel="shortcut icon" type="image/png" href={`${favicon}`}/>
+      <meta name="description" content="Mesilasemade kasvatus ja müük"/>
       {/*<meta name="image" content={image} />*/}
 
       {/* OpenGraph tags */}
-      <meta property="og:url" content="https://emadekasvatus.ee" />
-      <meta property="og:title" content="Mesilasemade kasvatus ja müük" />
-      <meta property="og:description" content="Mesilasemade kasvatus ja müük" />
+      <meta property="og:url" content="https://emadekasvatus.ee"/>
+      <meta property="og:title" content="Mesilasemade kasvatus ja müük"/>
+      <meta property="og:description" content="Mesilasemade kasvatus ja müük"/>
     </Helmet>
-    <Navbar />
+    <Navbar/>
     <div>{children}</div>
   </div>
-)
+);
 
-export default TemplateWrapper
+export default TemplateWrapper;
