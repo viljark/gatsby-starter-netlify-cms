7b8fe1de8aeabf3d2132139fc633609a18d3cfad
Viljar Kärgenberg <viljar@remato.com>
2023-04-30 00:25:52 +0300

    update ordering form

--- src/components/OrderingForm.js
+++ src/components/OrderingForm.js
@@ -1,7 +1,81 @@
 import React from "react";
 import { FaBuilding, FaEnvelope } from "react-icons/fa";
 
-export const OrderingForm = ({ active, notActiveDescription, heading, description }) => (
+export const OrderingForm = ({ active, notActiveDescription, heading, description }) =>{ 
+  const today = new Date();
+  const year = today.getFullYear();
+
+  // Find the first Monday of the year
+  const firstDayOfYear = new Date(year, 0, 1);
+  const firstMonday = new Date(
+    year,
+    0,
+    7 - (firstDayOfYear.getDay() + 6) % 7
+  );
+
+  // Find the current week number and start date
+  const currentWeekNumber = getWeekNumber(today, firstMonday);
+  let currentWeekStart = new Date(firstMonday.getTime());
+  currentWeekStart.setDate(currentWeekStart.getDate() + (currentWeekNumber - 1) * 7);
+
+  // Adjust current week's start date to the nearest Monday
+  while (currentWeekStart.getDay() !== 1) {
+    currentWeekStart.setDate(currentWeekStart.getDate() + 1);
+  }
+
+  // Initialize empty array of weeks
+  const weeks = [];
+
+  // Add options for each remaining week of the year
+  for (let i = currentWeekNumber; i <= 52; i++) {
+    const weekStart = new Date(firstMonday.getTime());
+    weekStart.setDate(weekStart.getDate() + (i - 1) * 7);
+
+    // Add one day to start on a Monday
+    weekStart.setDate(weekStart.getDate() + 1);
+
+    const weekEnd = new Date(weekStart.getTime());
+    weekEnd.setDate(weekEnd.getDate() + 6);
+
+    // Subtract one day to end on a Sunday
+    weekEnd.setDate(weekEnd.getDate());
+
+    if (i === currentWeekNumber) {
+      // Adjust current week's end date to the nearest Sunday
+      while (weekEnd.getDay() !== 0) {
+        weekEnd.setDate(weekEnd.getDate() + 1);
+      }
+
+      // Adjust current week's start date to the nearest Monday
+      while (currentWeekStart.getDay() !== 1) {
+        currentWeekStart.setDate(currentWeekStart.getDate() + 1);
+      }
+
+      // Add one day to current week's start date
+      currentWeekStart.setDate(currentWeekStart.getDate() + 1);
+    }
+
+    const monthFormatOptions = { month: "long" };
+    const label = `${weekStart.getDate()}. ${weekStart.toLocaleString(
+      "et-EE",
+      monthFormatOptions
+    )} - ${weekEnd.getDate()}. ${weekEnd.toLocaleString(
+      "et-EE",
+      monthFormatOptions
+    )}`;
+
+    const value = `${weekStart.toISOString()}_${weekEnd.toISOString()}`;
+    weeks.push({ label, value });
+  }
+
+  function getWeekNumber(date, firstMonday) {
+    const daysSinceFirstMonday = Math.floor(
+      (date.getTime() - firstMonday.getTime()) / (1000 * 60 * 60 * 24)
+    );
+    return 1 + Math.floor(daysSinceFirstMonday / 7);
+  }
+  
+ return (
   <div className="content box" id="tellimine">
     <h3 className="">
       {heading || "Mesilasemade tellimine"}
@@ -19,6 +93,21 @@ export const OrderingForm = ({ active, notActiveDescription, heading, descriptio
           <label className="label">Mesilasemade arv</label>
           <div className="control">
             <input required={true} name="emadeArv" className="input" type="text" placeholder=""/>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Kättesaamise aeg</label>
+          <div className="control">
+            <div className="select">
+            <select name="kättesaamiseAeg" required defaultValue={""}>
+              <option value="">-- Vali kättesaamise aeg --</option>
+              {weeks.map((week, index) => (
+                <option key={index} value={week.label}>
+                  {week.label}
+                </option>
+              ))}
+            </select>
+            </div>
           </div>
         </div>
         <div className="field">
@@ -89,4 +178,4 @@ export const OrderingForm = ({ active, notActiveDescription, heading, descriptio
       </article>
       }
   </div>
-);
+)};
